876b24681235fc5c28615c64e0c9d705d4f10939
Sadik Rahman <sadikur.ca.rahman@gmail.com>
2026-01-01 18:12:34 -0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-console.log("hello world!")
+console.log("hello world")
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
